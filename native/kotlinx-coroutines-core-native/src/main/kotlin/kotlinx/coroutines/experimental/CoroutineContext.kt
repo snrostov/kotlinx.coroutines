41ab9f6b2d6bbbfa15ecb1676e055e34856436f8
Roman Elizarov <elizarov@gmail.com>
2018-04-11 13:49:06 +0300

    native port work in progress

---
+++ native/kotlinx-coroutines-core-native/src/main/kotlin/kotlinx/coroutines/experimental/CoroutineContext.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2016-2017 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package kotlinx.coroutines.experimental
+
+import kotlin.browser.*
+import kotlin.coroutines.experimental.*
+
+private external val navigator: dynamic
+private const val UNDEFINED = "undefined"
+
+/**
+ * This is the default [CoroutineDispatcher] that is used by all standard builders like
+ * [launch], [async], etc if no dispatcher nor any other [ContinuationInterceptor] is specified in their context.
+ */
+public actual val DefaultDispatcher: CoroutineDispatcher = TODO()
+
+internal actual val DefaultDelay: Delay = DefaultDispatcher as Delay
+
+/**
+ * Creates context for the new coroutine. It installs [DefaultDispatcher] when no other dispatcher nor
+ * [ContinuationInterceptor] is specified, and adds optional support for debugging facilities (when turned on).
+ */
+public actual fun newCoroutineContext(context: CoroutineContext, parent: Job? = null): CoroutineContext {
+    val wp = if (parent == null) context else context + parent
+    return if (context !== DefaultDispatcher && context[ContinuationInterceptor] == null)
+        wp + DefaultDispatcher else wp
+}
+
+// No debugging facilities on native
+internal actual inline fun <T> withCoroutineContext(context: CoroutineContext, block: () -> T): T = block()
+internal actual fun Continuation<*>.toDebugString(): String = toString()
+internal actual val CoroutineContext.coroutineName: String? get() = null // not supported on native
